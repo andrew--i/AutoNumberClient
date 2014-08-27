b02af30ac73a8f4f41e6f27b9f79d472bb29df5c
Andrew <a--i@list.ru>
2014-08-27 19:16:43 +0400

    взаимодействие с сервером

--- MyApplication/app/src/main/java/ai/autonumber/CameraTestActivity.java
+++ MyApplication/app/src/main/java/ai/autonumber/CameraTestActivity.java
@@ -1,7 +1,5 @@
 package ai.autonumber;
 
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -9,7 +7,6 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.KeyEvent;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -23,18 +20,18 @@ import java.io.File;
 import java.util.Random;
 
 import ai.autonumber.adapter.ChatItemsArrayAdapter;
+import ai.autonumber.gcm.GoogleCloudMessageActiviti;
 import ai.autonumber.message.ChatMessage;
 import ai.autonumber.state.ActivitiState;
 
 
-public class CameraTestActivity extends Activity {
+public class CameraTestActivity extends GoogleCloudMessageActiviti {
     private static final int PHOTO_INTENT_REQUEST_CODE = 123;
-    private Context context;
     private Uri mUri;
     private ActivitiState currentActivitiState;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public   void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_test);
         updateAppearance(ActivitiState.MAIN_STATE);
@@ -84,6 +81,11 @@ public class CameraTestActivity extends Activity {
                 return false;
             }
         });
+    }
+
+    @Override
+    protected String getMainActivitiClassName() {
+        return CameraTestActivity.class.getSimpleName();
     }
 
     private void addChatMessageFromInput() {
@@ -139,26 +141,12 @@ public class CameraTestActivity extends Activity {
         }
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.camera_test, menu);
         return true;
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
 
     private Uri generateFileUri() {
         // Проверяем доступность SD карты
@@ -183,7 +171,6 @@ public class CameraTestActivity extends Activity {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == PHOTO_INTENT_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
-                //Log.w ("MY", "bitmap: " + data.getExtras().get("data"));
                 ((ImageView) findViewById(R.id.camPreview)).setImageURI(mUri);
                 updateAppearance(ActivitiState.CAM_STATE);
 
